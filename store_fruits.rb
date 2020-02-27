e1065d7885b5d509a3428ee2e3e43ecd123213f6
ARIANY <ariany_mendes@yahoo.com>
2020-02-27 17:00:51 -0500

    add atter and writter

--- store_fruits.rb
+++ store_fruits.rb
@@ -1,13 +1,3 @@
-#store_fruits = [
- # {:fruit1 => "apple", :color => "yellow", :price => "$1/lb" },
-  #{:fruit2 => "grape", :color => "green", :price => "$2/lb"},
-  #{:fruit3 => "peach", :color => "pink", :price => "$4/lb"}
-
-#]
-
-#p store_fruits
-
-
 
 class Store 
   
